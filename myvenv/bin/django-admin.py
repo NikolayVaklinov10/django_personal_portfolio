1d0020e030827f6b9589ebd65a4720d0ac9141a8
Nick <nikolayvaklinov@yahoo.com>
2019-08-20 23:14:43 +0100

    initial commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nikolayvaklinov/Desktop/Music/GitHub_Projects/Django_Projects/django_personal_portfolio-project/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myvenv/lib/python3.7/posixpath.py
@@ -0,0 +1,1 @@
+/Users/nikolayvaklinov/anaconda3/lib/python3.7/posixpath.py
