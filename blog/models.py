f873ee3f6f5e641cdccf6a8c6624a0ae821b94cc
Nick <nikolayvaklinov@yahoo.com>
2019-08-22 12:43:16 +0100

    added bootstap and several  jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()  # it allows more text to be enter than CharField this is the reason for picking it
+    image = models.ImageField(upload_to='images/')
