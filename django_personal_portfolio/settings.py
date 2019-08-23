cf7c5f9e758f8de2db3dbc6edb0c3ce88bd0fd1b
Nick <nikolayvaklinov@yahoo.com>
2019-08-23 15:50:54 +0100

    settings file

--- django_personal_portfolio/settings.py
+++ django_personal_portfolio/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfiliodb',
-        'USER':'postgres',
+        'USER':'nick',
         'PASSWORD':'Ne1Ym9Ar92',
         'HOST':'localhost',
         'PORT':'5432',
